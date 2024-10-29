29d4d858365dd5f25016a07f9a43db5e37a7bf37
ajojose803 <ajojose803@gmail.com>
2024-10-29 23:58:01 +0530

    Updated View posts, postsRow

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import './App.css';
 
-/**
- * ?  =====Import Components=====
- */
+import './App.css';
+import React, { useContext, useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import Home from './Pages/Home';
+import Signup from './Pages/Signup'
+import Login from './Pages/Login'
+import Create from './Pages/Create'
+import ViewPost from './Pages/ViewPost'
+import { AuthContext, FirebaseContext } from './store/Context';
+import Post from './store/PostContext';
 
 function App() {
+  const { setUser } = useContext(AuthContext);
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      setUser(user)
+    })
+  })
+
   return (
     <div>
-      <Home />
+      <Post>
+
+        <BrowserRouter>
+          <Routes>
+
+            <Route path='/' element={<Home />} />
+
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/create' element={<Create />} />
+            <Route path='/view' element={<ViewPost />} />
+
+          </Routes>
+        </BrowserRouter>
+      </Post>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import { FirebaseContext } from './store/Context';
+import firebase from './firebase/config';
+import Context from './store/Context';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
+    <React.StrictMode>
+        <FirebaseContext.Provider value={{ firebase }}>
+            <Context>
+                <App />
+            </Context>
+        </FirebaseContext.Provider>
+    </React.StrictMode>
+);
