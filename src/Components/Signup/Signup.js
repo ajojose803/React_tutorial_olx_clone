a9810fd92eee29dcff4a7f270a797c5a3c2564c4
ajojose803 <ajojose803@gmail.com>
2024-10-29 23:56:57 +0530

    Signup Created

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,58 +1,89 @@
-import React from 'react';
-
+import React, { useState, useContext } from 'react';
 import Logo from '../../olx-logo.png';
 import './Signup.css';
+import { FirebaseContext } from '../../store/Context';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Signup() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+ 
+  const navigate = useNavigate();
+  const { firebase } = useContext(FirebaseContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        result.user.updateProfile({ displayName: username })
+          .then(() => {
+            firebase.firestore().collection('users')
+              .add({
+                id: result.user.uid,
+                username: username,
+                phone: phone
+              }).then(() => {
+                navigate('/login');  // Redirect to login after successful signup
+              })
+          })
+      })
+  }
+
   return (
     <div>
       <div className="signupParentDiv">
-        <img width="200px" height="200px" src={Logo}></img>
-        <form>
+        <img width="200px" height="200px" src={Logo} alt="OLX Logo" />
+        <form onSubmit={handleSubmit}>
           <label htmlFor="fname">Username</label>
           <br />
           <input
             className="input"
             type="text"
             id="fname"
+            value={username} // updated
+            onChange={(e) => setUsername(e.target.value)}
             name="name"
-            defaultValue="John"
           />
           <br />
-          <label htmlFor="fname">Email</label>
+          <label htmlFor="email">Email</label>
           <br />
           <input
             className="input"
             type="email"
-            id="fname"
+            value={email} // updated
+            onChange={(e) => setEmail(e.target.value)}
+            id="email"
             name="email"
-            defaultValue="John"
           />
           <br />
-          <label htmlFor="lname">Phone</label>
+          <label htmlFor="phone">Phone</label>
           <br />
           <input
             className="input"
-            type="number"
-            id="lname"
+            type="text"
+            value={phone} // updated
+            onChange={(e) => setPhone(e.target.value)}
+            id="phone"
             name="phone"
-            defaultValue="Doe"
           />
           <br />
-          <label htmlFor="lname">Password</label>
+          <label htmlFor="password">Password</label>
           <br />
           <input
             className="input"
             type="password"
-            id="lname"
+            value={password} // updated
+            onChange={(e) => setPassword(e.target.value)}
+            id="password"
             name="password"
-            defaultValue="Doe"
           />
           <br />
           <br />
-          <button>Signup</button>
+          <button type="submit">Signup</button>
         </form>
-        <a>Login</a>
+        <Link to="/login">Login</Link>
       </div>
     </div>
   );
